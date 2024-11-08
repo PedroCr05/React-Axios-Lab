826378289dbf2197b62582951b97f0eeb4bc3481
pedrocruz <JayJMarquez05@gmail.com>
2024-11-07 19:25:30 -0600

    Getting data from other parts of the database. Now that's cool! :D

--- src/components/ComponentDetails/CharacterDetails.jsx
+++ src/components/ComponentDetails/CharacterDetails.jsx
@@ -12,10 +12,15 @@ const CharacterDetails = (props) => {
     const getCharacter = async () => {
       try {
         const res = await axios.get(`${BASE_URL}/people`);
-        setCharacter(res.data.results[characterId]);
+        // To get the information from other parts of the the data base let's make sure to pass it through a proper variable.
+        const resCharacter = res.data.results[characterId];
+        // From there I can pass it through the setting of our useState function so it can show up
+        setCharacter(resCharacter);
 
-        if (character.homeworld) {
-          const res = await axios.get(character.homeworld);
+        // If we do plan to use this. I MUST pass a conditional so this code doesn't run before our first api get call
+        if (resCharacter.homeworld) {
+          // Only key difference is we get our variable call & the tag by using dot notation
+          const res = await axios.get(resCharacter.homeworld);
           setHomeworld(res);
         }
       } catch (e) {
@@ -24,16 +29,17 @@ const CharacterDetails = (props) => {
     };
     getCharacter();
   }, []);
-
-  console.log(homeworld);
+  // To double check if the useState for our variable that we care calling from another part within the database run the console log
+  // console.log(homeworld);
   return (
     <>
       <h2>{character.name}</h2>
       <dl>
         <dt>Home World</dt>
-        <dd></dd>
+        {/* When we do want to call it. Let's use dot notation so it does pass through! */}
+        <dd>{homeworld.data.name}</dd>
         {/* When I get the chance. For sure ask how to do a back and forth link.
-        That'd be nice to understand! */}
+        That'd be nice to understand! - Complete! YAY learned something new hehe*/}
         <dt>Birth Year</dt>
         <dd>{character.birth_year}</dd>
         <dt>Height</dt>
